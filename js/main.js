3916f2623fdf2a1f216b7492e098e56bec96aa1c
Irvin Cortez <irvincotez67@gmail.com>
2020-11-15 17:45:30 -0600

    ventana flotante terminada

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-$('#carrusel').carousel({
-    touch: true
-})
+
+function mostrarVentana(elemento){
+    let ventana = document.getElementById("ventana");
+    let clase = ventana.className;
+    ventana.className = clase === "order" ? "order-show" : "order";
+}
